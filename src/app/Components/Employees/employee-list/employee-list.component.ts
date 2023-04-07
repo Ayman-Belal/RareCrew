74ac465ccf0595bc34fa54219744a3bb36fa0519
Ayman <aymansamir741@gmail.com>
2023-04-07 10:41:08 +0200

    edit calculation

--- src/app/Components/Employees/employee-list/employee-list.component.ts
+++ src/app/Components/Employees/employee-list/employee-list.component.ts
@@ -30,64 +30,7 @@ export class EmployeeListComponent {
       })
       
   }
-  // getEmployees(): void {
-  //   this.employeeService.getEmployees().subscribe((data: IEmployee[]) => {
-  //     // this.employees= data;
-  //     // const employeess = data.reduce((acc, entry) => {
-  //     //   if (acc[entry.EmployeeName]) {
-  //     //     acc[entry.EmployeeName].push(entry);
-  //     //   } else {
-  //     //     acc[entry.EmployeeName] = [entry];
-  //     //   }
-  //     //   return acc;
-  //     // }, {})
-
-  //     const result_items: { [name:string]: IEmployee [] } = {}
-     
-  //     data.forEach(item=>{
-  //       const name = item.EmployeeName  as string
-  //       if(result_items[name]){
-  //         result_items[name].push(item)
-  //       }else{
-  //         result_items[name] = [item];
-  //       }
-  //     })
-
-  //      // {
-  //      // 'karim' : [ob, ob2, ob3, ob4 ],
-  //     // 'ali' : [ob, ob2],
-  //     // 
-  //      //}
-
-  //      // []
-
-       
-  //     for(let key in result_items){
-  //       const items = result_items[key]
-  //       // cons
-  //       let timeMiliSec = 0;
-
-  //      items.forEach(item=>{
-  //         const attendacePerDay = new Date(item.EndTimeUtc).getTime()  - new Date ( item.StarTimeUtc).getTime();
-  //         timeMiliSec += attendacePerDay; // 100
-         
-        
-  //       })
-
-  //       this.employees.push({
-  //         EmployeeName: items[0].EmployeeName,
-  //         timeHours: Math.round( timeMiliSec / (1000 * 60 *60))
-  //       })
-  //       this.employees.sort((a,b)=>b.timeHours-a.timeHours);
-
-
-  //     }
-
-
-  //     // console.log(data);
-  //   });
-  // }
- 
+  
   isLessThan100Hours(employee: UIEmployee): boolean {
     return employee.timeHours < 100;
   }
